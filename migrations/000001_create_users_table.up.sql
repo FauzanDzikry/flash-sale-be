ad6504121c0a6dc24461a3c02ad76dc28dd19918
FauzanYBM <stf.programmer1@ybmbri.org>
2026-02-22 07:58:29 +0700

    change password 50 to 255

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email      VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     name       VARCHAR(100),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
