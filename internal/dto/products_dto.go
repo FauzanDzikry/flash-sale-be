77213ebbea5ce1340cfc25737207595e0acf1cfc
FauzanYBM <stf.programmer1@ybmbri.org>
2026-02-24 23:59:15 +0700

    Enhance validation rules for product creation and update requests by adding constraints for stock, price, and discount fields. Ensure stock and price must be non-negative, and discount must be between 0 and 100.

--- internal/dto/products_dto.go
+++ internal/dto/products_dto.go
@@ -18,16 +18,16 @@ type ProductResponse struct {
 type CreateProductRequest struct {
 	Name      string  `json:"name" binding:"required"`
 	Category  string  `json:"category" binding:"required"`
-	Stock     int     `json:"stock" binding:"required"`
-	Price     float64 `json:"price" binding:"required"`
-	Discount  float64 `json:"discount" binding:"required"`
+	Stock     int     `json:"stock" binding:"required,gte=0"`
+	Price     float64 `json:"price" binding:"required,gte=0"`
+	Discount  float64 `json:"discount" binding:"gte=0,lte=100"` // 0 diterima; hanya tidak boleh <0 atau >100
 	CreatedBy string  `json:"created_by" binding:"required"`
 }
 
 type UpdateProductRequest struct {
 	Name     string  `json:"name" binding:"required"`
 	Category string  `json:"category" binding:"required"`
-	Stock    int     `json:"stock" binding:"required"`
-	Price    float64 `json:"price" binding:"required"`
-	Discount float64 `json:"discount" binding:"required"`
+	Stock    int     `json:"stock" binding:"required,gte=0"`
+	Price    float64 `json:"price" binding:"required,gte=0"`
+	Discount float64 `json:"discount" binding:"gte=0,lte=100"` // 0 diterima
 }
